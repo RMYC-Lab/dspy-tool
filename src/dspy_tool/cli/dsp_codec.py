c1e8f430ce7c0ac2b067483dc6de3c6ed4afb3dc
Cranyozen <61766249+cranyozen@users.noreply.github.com>
2024-05-01 23:53:53 +0800

    Refactor dsp_codec.py CLI arguments for better readability and consistency

--- src/dspy_tool/cli/dsp_codec.py
+++ src/dspy_tool/cli/dsp_codec.py
@@ -2,17 +2,20 @@ import argparse
 import datetime
 import re
 from pathlib import Path
+from typing import Union, Tuple
+from urllib.parse import unquote
 
-from dspy_tool import __version__
 from dspy_tool.dsp_codec.file import DspFile
 
-# __version__ = "0.0.1"
+__version__ = "0.0.4"
 
 DEBUG = False
 
-DELETE_COMMENTS_RE = r"\n *?#block.+?\n"
+DELETE_COMMENTS_RE = r" *?#block.*?\n"
 
-PYTHON_CODE_RE = r"<python_code><!\[CDATA\[(.+?)\]\]><\/python_code>"
+PYTHON_CODE_RE = r"<python_code><!\[CDATA\[(.*?)\]\]><\/python_code>"
+
+CHINSES_RE = r"((_[0-9A-F]{2}){3})"
 
 
 def process_py_file(
@@ -24,7 +27,8 @@ def process_py_file(
     raw: bool,
     std_out: bool,
     delete_comments: bool,
-) -> None:
+    process_chinese: bool,
+) -> Union[str, None]:
     if not file_name:
         file_name = DspFile.get_file_name(input_file_path.name)
     if (not file_name.endswith(".dsp") and not raw) or (
@@ -36,18 +40,18 @@ def process_py_file(
     dsp_file = DspFile.new_with_python_code(creator, title, python_code, file_name)
     if delete_comments:
         python_code = dsp_file.get_python_code()
-        dsp_file.dji.code.python_code = re.sub(DELETE_COMMENTS_RE, "\n", python_code)
+        dsp_file.dji.code.python_code = re.sub(DELETE_COMMENTS_RE, "", python_code)
     if raw:
         if std_out:
-            print(dsp_file.dji.get_xml_string())
+            return dsp_file.dji.get_xml_string()
         else:
             if not file_name.endswith(".xml"):
                 file_name += "_raw.xml"
             with open(output_file_path / file_name, "wb") as file:
-                file.write(dsp_file.dji.get_xml_string().encode())
+                file.write(dsp_file.dji.get_xml_string().encode(encoding="utf-8"))
     else:
         if std_out:
-            print(dsp_file.get_dsp_data().decode(encoding="utf-8"))
+            return dsp_file.get_dsp_data().decode(encoding="utf-8")
         else:
             if not file_name.endswith(".dsp"):
                 file_name += ".dsp"
@@ -63,7 +67,8 @@ def process_dsp_file(
     raw: bool,
     std_out: bool,
     delete_comments: bool,
-) -> None:
+    process_chinese: bool,
+) -> Union[str, None]:
     if not file_name:
         file_name = DspFile.get_file_name(input_file_path.name)
     if (not file_name.endswith(".py") and not raw) or (
@@ -71,25 +76,46 @@ def process_dsp_file(
     ):
         file_name = f"{file_name}_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}"
     with input_file_path.open("rb") as file:
-        dsp_data = DspFile.decode_dsp(file.read()).decode(encoding="utf-8")
+        dsp_data = file.read().decode(encoding="utf-8")
+        if not dsp_data.startswith("<dji><attribute>"):
+            dsp_data = DspFile.decode_dsp(dsp_data.encode(encoding="utf-8")).decode(
+                encoding="utf-8"
+            )
     if delete_comments:
-        dsp_data = re.sub(DELETE_COMMENTS_RE, "\n", dsp_data)
+        dsp_data = re.sub(DELETE_COMMENTS_RE, "", dsp_data)
+    if process_chinese:
+        for line in dsp_data.split("\n"):
+            if decode_chinese(line)[0]:
+                dsp_data = dsp_data.replace(
+                    line, line + "  ## " + " -> ".join(decode_chinese(line))
+                )
     if raw:
         if std_out:
-            print(dsp_data)
+            return dsp_data
         else:
             if not file_name.endswith(".xml"):
                 file_name += "_raw.xml"
             with open(output_file_path / file_name, "wb") as file:
-                file.write(dsp_data.encode())
+                file.write(dsp_data.encode(encoding="utf-8"))
     else:
+        code = re.findall(PYTHON_CODE_RE, dsp_data, re.S)
+        if not code:
+            print("No python code found in the dsp file.")
+            return ""
         if std_out:
-            print(re.findall(PYTHON_CODE_RE, dsp_data, re.S)[0])
+            return code[0]
         else:
             if not file_name.endswith(".py"):
                 file_name += ".py"
             with open(output_file_path / file_name, "w", encoding="utf-8") as file:
-                file.write(re.findall(PYTHON_CODE_RE, dsp_data, re.S)[0])
+                file.write(code[0])
+
+
+def decode_chinese(data: str) -> Tuple[str, str]:
+    origin = re.findall(CHINSES_RE, data)
+    return "".join([i[0] for i in origin]), "".join(
+        [unquote(i[0].replace("_", "%")) for i in origin]
+    )
 
 
 def main():
@@ -119,6 +145,12 @@ def main():
         "--delete-comments",
         action="store_true",
         help="try to delete comments for blocks. (defaults to False)",
+    )
+    parser.add_argument(
+        "--pc",
+        "--process-chinese",
+        action="store_true",
+        help="try to decode chinese characters. (defaults to False)",
     )
     parser.add_argument(
         "-t",
@@ -159,7 +191,7 @@ def main():
             if not args.std_out:
                 if not output_file_path.exists():
                     output_file_path.mkdir(parents=True)
-            process_py_file(
+            ret = process_py_file(
                 input_file_path,
                 output_file_path,
                 args.file_name,
@@ -167,10 +199,13 @@ def main():
                 args.creator,
                 args.raw,
                 args.std_out,
-                args.delete_comments,
+                args.dc,
+                args.pc,
             )
+            if args.std_out:
+                print(ret)
         elif input_file_path.suffix == ".dsp":
-            process_dsp_file(
+            ret = process_dsp_file(
                 input_file_path,
                 output_file_path,
                 args.file_name,
@@ -178,8 +213,11 @@ def main():
                 args.creator,
                 args.raw,
                 args.std_out,
-                args.delete_comments,
+                args.dc,
+                args.pc,
             )
+            if args.std_out:
+                print(ret)
         else:
             raise ValueError(f"The input file is not a valid file. Path: {args.input}")
 
